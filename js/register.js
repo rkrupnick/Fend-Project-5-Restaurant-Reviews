499fb7a3acea63fee1405bddc58a424ce34df724
Rachel Krupnick <rachel@rachelkrupnick.com>
2018-07-30 16:07:32 -0400

    Add service worker registration

---
+++ js/register.js
@@ -0,0 +1,12 @@
+/* Set up Service Worker */
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(reg => {
+      console.log('Service worker registration successful: ' + reg.scope);
+    })
+    .catch(error => {
+      console.log("Registration failed: " + error);
+    });
+}
