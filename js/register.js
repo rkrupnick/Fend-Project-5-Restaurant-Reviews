4d0a43c18a7e35671657a775c807d242938a48ce
Rachel Krupnick <rachel@rachelkrupnick.com>
2018-08-01 21:18:09 -0400

    Add blank service worker and registration

--- js/register.js
+++ js/register.js
@@ -2,7 +2,7 @@
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register('/sw.js', {scope: '/'})
+    .register('sw.js')
     .then(reg => {
       console.log('Service worker registration successful: ' + reg.scope);
     })
